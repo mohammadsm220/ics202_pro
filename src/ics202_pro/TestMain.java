044f0507b49abb66da6eab4ab553ce621d46dda5
Omer-code <55999571+Omer-code@users.noreply.github.com>
2020-04-30 14:26:22 +0300

    change 1

--- src/ics202_pro/TestMain.java
+++ src/ics202_pro/TestMain.java
@@ -11,19 +11,21 @@ public class TestMain {
 
 		t1.insert(str);
 
-		System.out.println(t1);
+
 
 		t1.insert("side");
 		t1.insert("sid");
 		System.out.println(t1.contains("side"));
-
+		System.out.println(t1.size);
 		System.out.println(t1.contains("sid"));
 
 		System.out.println(t1.contains("cat"));
 		t1.delete("sid");
 		t1.delete("side");
+		System.out.println(t1.size);
 		System.out.println(t1.contains("side"));
 		System.out.println(t1.contains("sid"));
+
 
 
 
